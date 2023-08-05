7bf3ee93a43289c42ba5f59dd7e5dee1cb904375
Cheeserico <135801979+Cheeserico@users.noreply.github.com>
2023-08-05 19:52:58 +0900

    スラッシュ途中

--- Assets/Scripts/SlashSpawnerController.cs
+++ Assets/Scripts/SlashSpawnerController.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlashSpawnerController : MonoBehaviour
+// ベースを継承するように（モノビヘイビア消す）
+// 武器のプレハブをインスペクターから選択できるようになる
+public class SlashSpawnerController : BaseWeaponSpawner
 {
     // Start is called before the first frame update
     void Start()
